95c9484f923d0121ff561d5b0cb6e2b5917aeb0e
Ananyasolanki123 <133773293+Ananyasolanki123@users.noreply.github.com>
2024-03-30 16:30:10 +0000

    types

--- .devcontainer/basics/types.js
+++ .devcontainer/basics/types.js
@@ -10,3 +10,20 @@ const myfunction=function(){
     console.log("hi");
 }
 console.log(typeof hero)
+/******************************************************** */
+//stack(primitive),heap(non -primitive)
+let myname ="annaya"
+let anothername= myname;
+anothername="sadkasjk"
+console.log(anothername);
+console.log(myname);
+console.log(heap);
+let userone={
+    user:"hdhsukl",
+    email:"dkjkdj",
+}
+let usertwo= userone;
+usertwo.user="wkdkdsj"
+console.log(userone.user)
+console.log(usertwo.user)
+
