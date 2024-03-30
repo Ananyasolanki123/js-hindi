e2f17b34a22a90fcc3a32b0249679b7e817a3a02
Ananyasolanki123 <133773293+Ananyasolanki123@users.noreply.github.com>
2024-03-30 16:13:10 +0000

    types

---
+++ .devcontainer/basics/types.js
@@ -0,0 +1,12 @@
+//array
+const hero=["anjsn","jskjand","bashbajh"];
+//obj
+let myobj={
+    name:"Ani",
+    age:6
+
+}
+const myfunction=function(){
+    console.log("hi");
+}
+console.log(typeof hero)
