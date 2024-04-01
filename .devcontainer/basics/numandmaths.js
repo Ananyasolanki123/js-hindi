9a4bc2acf2f82e204cf85c456aafd5f1d9791046
Ananyasolanki123 <133773293+Ananyasolanki123@users.noreply.github.com>
2024-04-01 13:35:34 +0000

    maths done

---
+++ .devcontainer/basics/numandmaths.js
@@ -0,0 +1,15 @@
+const balance =new Number(100)
+console.log(balance)
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+const othernumber=1123.8966
+console.log(othernumber.toPrecision(3));
+console.log(othernumber.toPrecision(3));
+console.log(othernumber.toLocaleString());
+console.log(othernumber.toLocaleString('en-IN'));
+/********************************************MATHS*************************************** */
+const min =10
+const max=20
+console.log(Math.floor(Math.random()*(max - min +1))+min)
+
+
