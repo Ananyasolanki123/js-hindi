3ea4d083a80a56b1f689ee6a813876206d183a45
Ananyasolanki123 <133773293+Ananyasolanki123@users.noreply.github.com>
2024-04-01 12:55:49 +0000

    strings done

---
+++ .devcontainer/basics/strings.js
@@ -0,0 +1,20 @@
+const name ="ananya"
+const repcount=50;
+//console.log(name+ repcount);
+console.log(`heloo my name is${name} and my repo count is ${repcount}`);
+const gamename = new String("hiteshc")
+console.log(gamename.__proto__);
+console.log(gamename.toUpperCase());
+console.log(gamename.charAt(4));
+console.log(gamename.indexOf('s'));
+const newstring= gamename.substring(0,4)//negative value cannot be used
+console.log(newstring);
+const anotherstring= gamename.slice(-8,4)
+console.log(anotherstring);
+const newStringone= "       hitesh      "
+console.log(newStringone);
+console.log(newStringone.trim())
+
+
+
+
