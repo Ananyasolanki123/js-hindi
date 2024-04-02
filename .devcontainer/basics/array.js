e1b0d5b8024c775348284c990f858e85d6b94cf4
Ananyasolanki123 <133773293+Ananyasolanki123@users.noreply.github.com>
2024-04-02 15:08:31 +0000

    array

--- .devcontainer/basics/array.js
+++ .devcontainer/basics/array.js
@@ -39,6 +39,23 @@ const myn2 = myarr.splice(1,3);
 console.log("C",myarr);
 
 console.log(myn2);
+//instead of concatination we can use spread function
+const marvel_heros=["thor","asbashahs","hulk"];
+const dc_heroes=["ananya","asit","arpit"];
+const all_new_heroes=[...marvel_heros,...dc_heroes]
+console.log(all_new_heroes);
+const another_array=[1,2,3,[4,5,6],7,[6,7,[4,5]]]
+const real_another_array=another_array.flat(Infinity);
+console.log(real_another_array);
+console.log(Array.isArray("ananya"))
+console.log(Array.from("ananya"))
+console.log(Array.from({name:"ananya"}))//interesting case
+let score1=100;
+let score2=200;
+let score3=300;
+console.log(Array.of(score1,score2,score3));
+
+
 
 
 
