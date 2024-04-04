aa2295b2c5c0bb14c3eec47ea556a7a33dde240c
Ananyasolanki123 <133773293+Ananyasolanki123@users.noreply.github.com>
2024-04-04 16:31:13 +0000

    scope

---
+++ .devcontainer/basics/scope.js
@@ -0,0 +1,20 @@
+function one(){
+    const username="snj"
+    function two(){
+        const website="yt"
+        console.log(username);
+    }
+    //console.log(website);
+     two();
+
+}
+one();
+console.log(addone(5))
+function addone(num){
+    return num+1
+}
+addtwo(5)
+const addtwo=function(num){
+    return num +2;
+}
+//addtwo(5)
