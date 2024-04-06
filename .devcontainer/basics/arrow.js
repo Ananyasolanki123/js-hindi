dd7e86ee511b377a67d50f07a3a9111f5db33b2f
Ananyasolanki123 <133773293+Ananyasolanki123@users.noreply.github.com>
2024-04-06 00:20:42 +0000

    arrow done

--- .devcontainer/basics/arrow.js
+++ .devcontainer/basics/arrow.js
@@ -9,4 +9,29 @@ const user={
 //user.welcomemessage()
 //user.username="aaaaa"
 //user.welcomemessage()
-console.log(this)
+//console.log(this)
+/*function chai(){
+   let username="chh"
+    console.log(this.username)
+}
+//containerchai()*/
+/*const chai=function (){
+    let username="chh"
+     console.log(this.username)
+ }
+ chai()*/
+ const chai=()=>{
+    let username="chh"
+     console.log(this.username)
+ }
+ chai()
+ /*const add=(num1,num2)=>{
+    return num1 + num2;
+ }
+ console.log(add(3,4))*/
+ const add=(num1,num2)=> (num1 + num2)
+ 
+ console.log(add(3,4))
+ const two=(num1,num2)=> ({username:"ananya"})
+ 
+ console.log(two(3,4))
