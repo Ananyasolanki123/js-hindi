6cd0ec750486dff2fd25c2e8674c65392ed9fa91
Ananyasolanki123 <133773293+Ananyasolanki123@users.noreply.github.com>
2024-04-07 01:55:36 +0000

    switch

---
+++ .devcontainer/controlflow/switch.js
@@ -0,0 +1,11 @@
+//falsy values 
+//false,0,-0,bigInt,0n,"",null,undefined,NaN
+//truthy values
+//true,"0",'false'," ",[],{},function(){}
+//Nullish Coalescing operator(??):null undefined
+let val1;
+val1=5?? 10
+console.log(val1);
+console.log(val1);
+console.log(val1);
+console.log(val1);
