cc796fddf4ee5a9717f610bf25b204ef544d868a
Ananyasolanki123 <133773293+Ananyasolanki123@users.noreply.github.com>
2024-04-19 11:52:54 +0000

    promises

--- advance_one/promises.js
+++ advance_one/promises.js
@@ -48,3 +48,43 @@ promisefour.then((user)=>{
 }).finally(()=>{
     console.log("promise is finally resloved or reject")
 })
+const promisefive=new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error=true
+        if(!error){
+            resolve({username:"ananya",passwors:"george"})
+        }else{
+            reject('error:js ent wrong')
+        }
+
+    },1000)
+})
+async function consumepromisefive(){
+    try{
+        const response=await promisefive
+    console.log(response);
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+consumepromisefive()
+async function getusers(){
+    try{
+        const response=await fetch('https://jsonplaceholder.typicode.com/users')
+    const data= await response.json()
+    console.log(data);
+    }
+    catch(error){
+        console.log("E:",error);
+    }
+}
+getusers()
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response)=>{
+    return response.json()
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch((error)=>console.log(error))
