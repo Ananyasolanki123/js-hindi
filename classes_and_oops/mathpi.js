c0158a12f1eee1fcb6bb0e7ef5bc9b42c09c4715
Ananyasolanki123 <133773293+Ananyasolanki123@users.noreply.github.com>
2024-04-21 18:15:41 +0000

    change in property

--- classes_and_oops/mathpi.js
+++ classes_and_oops/mathpi.js
@@ -3,12 +3,24 @@ console.log(discriptor);
 const chai ={
     name:"sss",
     price:56,
-    is:true
+    is:true,
+    orderchai:function(){
+        console.log("chai nhi bni");
+    }
 }
 console.log(Object.getOwnPropertyDescriptor(chai,"name"));
 Object.defineProperty(chai,'name',{
-    writable:false,
+   // writable:false,
     enumerable:false
+    //if we change enumerable to true than in iteration is done on name also
 })
 console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+for (const [key,value] of Object.entries(chai)) {
+    if(typeof value!=="function"){
+        console.log(`${key}:${value}`);
+
+    }
+    
+    
+}
 
